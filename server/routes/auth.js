d10bb74d0272b018fedf8362fa6effb062b35466
Dharmik Pandav <dbpandav@Dharmiks-MacBook-Air.local>
2026-09-12 18:46:40 +0530

    fix: Token expires update to 3h

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -80,7 +80,7 @@ function generateToken(user) {
   return jwt.sign(
     { id: user.id, email: user.email, name: user.name, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '12h' }
+    { expiresIn: '2h' }
   );
 }
 
